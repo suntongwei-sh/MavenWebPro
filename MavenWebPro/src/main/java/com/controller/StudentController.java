33dc7f0eebbc1a847745447733db1fe07de04181
suntw <suntongweimy@163.com>
2016-09-19 14:51:01 +0800

    9.19

--- MavenWebPro/src/main/java/com/controller/StudentController.java
+++ MavenWebPro/src/main/java/com/controller/StudentController.java
@@ -3,13 +3,23 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.entity.Student;
 
 @Controller
 @RequestMapping("/student.do")
 public class StudentController {
 	@RequestMapping(method = RequestMethod.GET)
-	public void test_1() {
+	@ResponseBody
+	public Student test_1() {
+		Student student = new Student();
+		student.setAge("14");
+		student.setId(12);
+		student.setName("AAA");
+		student.setSex("male");
 		System.out.println("我是get请求");
+		return student;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
