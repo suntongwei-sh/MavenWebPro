ae33663646306630b6994e02652e8c1b89f2b681
suntw <suntongweimy@163.com>
2016-10-08 13:05:23 +0800

    10.8

--- MavenWebPro/src/main/java/com/controller/TestController.java
+++ MavenWebPro/src/main/java/com/controller/TestController.java
@@ -7,15 +7,19 @@ import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.entity.Response;
 import com.entity.Student;
 
 @RequestMapping("/test/")
 @Controller
 public class TestController {
-	@RequestMapping("index_1.do")
+	@RequestMapping(value = "index_1.do", method = RequestMethod.POST)
 	public ModelAndView index_1() {
+		Response response = new Response();
+		response.success("gdfgfgd");
 		JSONObject jo = new JSONObject();
 		ModelAndView mv = new ModelAndView();
 		List<Student> list = new ArrayList<Student>();
@@ -24,6 +28,7 @@ public class TestController {
 		jo.put("student", student);
 		jo.put("list", list);
 		jo.put("number", 5434);
+		jo.put("response", response);
 		mv.addObject("jsonst", jo.toString());
 		return mv;
 
